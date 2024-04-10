24c7136d0f7b9112aab495a04f730074839657ab
parthtyagi9 <129141290+parthtyagi9@users.noreply.github.com>
2024-04-09 23:03:49 -0400

    Delete assignments/a2/starter_code/Test_cases directory

--- assignments/a2/starter_code/Test_cases/py_files/python2.py
+++
@@ -1,6 +0,0 @@
-Python 3.10.11 (v3.10.11:7d4cc5aa85, Apr  4 2023, 19:05:19) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a = 5
->>> b = 5
->>> print(a + b)
-10
